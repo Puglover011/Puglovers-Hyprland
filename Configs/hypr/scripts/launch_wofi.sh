4b2af77cf3586e2d9a37c542724f908f431f4a85
Puglover011 <162393909+Puglover011@users.noreply.github.com>
2025-09-22 01:29:19 +1000

    Delete Configs directory in preperation for new directory.

--- Configs/hypr/scripts/launch_wofi.sh
+++
@@ -1,2 +0,0 @@
-#!//usr/bin/env sh
-wofi --show drun
--- Configs/waybar/scripts/power_menu.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# Simple menu using wofi
-choice=$(printf "Performance\nBalanced\nPower Saver" | wofi --dmenu --prompt "Select profile")
-case "$choice" in
-    Performance)  powerprofilesctl set performance ;;
-    Balanced)     powerprofilesctl set balanced ;;
-    "Power Saver") powerprofilesctl set power-saver ;;
-esac
